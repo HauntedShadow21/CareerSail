58713368101d254d46438237946ab2705bf51d11
HauntedShadow21 <hauntedshadow21@gmail.com>
2016-09-03 16:26:07 -0700

    Update 3

--- custom.js
+++ custom.js
@@ -31,6 +31,7 @@ function getIndeedResponse(query){
 function processResponse(xml){
   var xmlDocument = xml.responseXML;
   var result;
+  var items = [];
   var results = xml.getElementsByTagName("RESULTS");
   firstResult= results.getElementsByTagName("RESULT")[0];
   secondResult= results.getElementsByTagName("RESULT")[1];
@@ -58,28 +59,46 @@ function processResponse(xml){
   }
 }
 /*
-  @description: Gets average salaries based on input
+  @description: Gets average salaries based on query
 */  
-function getAverageSalary(query){
+function getAverageWorkLife(query){
+  $.getJSON(query, function(data){
+      $.each(data, function(key,val){
+        if(key=="workLifeBalanceRating"){
+          items.push(Double(val));
 
+        }
+      });
+  });
+  var counter = 0;
+  var totalnum = 0;
+  var avg = 0;
+  for(i in items){
+    counter+=items[i];
+    totalnum++; 
+  }
+  avg = counter/items;
 }
-/*
-  @description: Gets id of top three videos from query
+
+/**
+  @description: Gets id of top three videos from query in YouTube
 */
 function getIds(query){
-
+  var results = You
 }
 /*
   @description: Creates three iframes that display YouTube videos given an ID.
 */
-function displayVideos(id1, id2, id3){
-
+var exampleVideoIDs = ["JkR7xlYjalg"];
+function displayVideos(videos){
+  for(i in videos){
+    document.write("<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/TzOfrY0EFtQ\" frameborder=\"0\" allowfullscreen></iframe>");
+  }
 }
 /*
   @description: Gets list of top three colleges 
 */
 function getListOfBestColleges(query){
-  
+ 
 }
-
-
+getAverageWorkLife("http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=91513&t.k=heqCc4gY3nk&action=employers&q="+ "example");
